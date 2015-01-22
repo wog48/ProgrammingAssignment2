c5a5bbb5496c3aed7cb504ad9826ef407dfaa72f
Oliver Grande <oliver.grande@alice-dsl.net>
2015-01-22 21:57:28 +0100

    First try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
+## Task of 
 ## functions do
+# First Git Test Version without commands
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Initialise inverse matrix buffer
+	im <- NULL
+	# 
+	set <- function(y){  x <<- y ; im <<- NULL }
+	get <- function() { x }
+	setInverse <- function( invers ){ im <<- invers }
+	getInverse <- function( ){ im }
+	list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+    ## Return a matrix that is the inverse of 'x'
+	if ( is.null(x$getInverse())){
+		#print("calculate")
+		x$setInverse( solve(x$get(),...))
+	}
+	x$getInverse()
 }
